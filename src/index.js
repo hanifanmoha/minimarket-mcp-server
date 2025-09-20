ba116ad74606fdb34f75a6b156cc69c93695dc5c
Mohamad Hanifan <mohamad.hanifan@quipper.com>
2025-09-20 23:45:46 +0700

    import express

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// importing express so that vercel detect this project as function
+import express from "express";
 import { createHTTPServer } from "./http-server.js";
 import { mcpServer } from "./mcp-server.js";
 
